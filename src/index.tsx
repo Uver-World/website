bcff5c382d06a1699e9bbbb2d402a44bfaef7980
Clément <clement.ozor@protonmail.com>
2022-09-12 10:44:27 +0200

    [main] => Install and configure ReactRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/globals.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome to Uver World!</h1>
+    </div>
+  );
+}
+
+export default Home;
