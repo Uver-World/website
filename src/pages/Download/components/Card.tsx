65edf6381418e2fe78431bba59d7e0ec7754ccf9
Clément <clement.ozor@protonmail.com>
2022-09-15 12:34:32 +0200

    [main] => Add OS detection

--- src/pages/Download/components/Card.tsx
+++ src/pages/Download/components/Card.tsx
@@ -8,14 +8,15 @@ type CardProps = {
   appFile: string;
   appName: string;
   version: string;
+  isActive?: boolean;
   children: React.ReactNode;
 }
 
-const Card: FunctionComponent<CardProps> = ({title, appFile, appName, version, children}) => {
+const Card: FunctionComponent<CardProps> = ({title, appFile, appName, version, isActive = false, children}) => {
   const navigate = useNavigate();
 
   return (
-    <article className={`${styles.card} dark:bg-slate-500`}>
+    <article className={`${styles.card} dark:bg-slate-500 ${isActive ? `${styles.isActive} dark:bg-blue-500` : null}`}>
       <div className={styles.iconContainer}>
         { children }
         <h2 className={styles.subtitle}>{title}</h2>
@@ -27,7 +28,7 @@ const Card: FunctionComponent<CardProps> = ({title, appFile, appName, version, c
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
           </svg>
         </a>
-        <p className={`${styles.version} dark:text-slate-200`}>Version: {version}</p>
+        <p className={`${styles.version} dark:text-slate-200 ${isActive ? styles.versionIsActive : null}`}>Version: {version}</p>
       </div>
     </article>
   );
--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,6 +1,7 @@
 import styles from './styles/index.module.css';
 import download from "../../assets/img/background.jpg";
 import Card from "./components/Card";
+import {getOS} from "../../utils/getOS";
 
 const Download = () => {
   return (
@@ -8,19 +9,19 @@ const Download = () => {
       <h1 className={styles.title}>Version d'essai</h1>
       <div className={styles.downloadContainer}>
 
-        <Card title="Windows" appFile={download} appName="UverWorld.exe" version="1.0.2">
+        <Card isActive={getOS(window) === 'Windows OS'} title="Windows" appFile={download} appName="UverWorld.exe" version="1.0.2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 8.25V18a2.25 2.25 0 002.25 2.25h13.5A2.25 2.25 0 0021 18V8.25m-18 0V6a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6zM7.5 6h.008v.008H7.5V6zm2.25 0h.008v.008H9.75V6z" />
           </svg>
         </Card>
 
-        <Card title="MacOS" appFile={download} appName="UverWorld.dmg" version="1.0.1">
+        <Card isActive={getOS(window) === 'MacOS'} title="MacOS" appFile={download} appName="UverWorld.dmg" version="1.0.1">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
           </svg>
         </Card>
 
-        <Card title="Linux" appFile={download} appName="UverWorld.zip" version="1.0.13">
+        <Card isActive={getOS(window) === 'Linux OS' || getOS(window) === 'UNIX OS'} title="Linux" appFile={download} appName="UverWorld.zip" version="1.0.13">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
           </svg>
